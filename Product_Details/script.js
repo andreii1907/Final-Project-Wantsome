8817c3bf9d115e5d8abb0ee7df01d2b6174d0d7d
andreii1907 <luchianandrei1024@yahoo.com>
2021-05-21 23:53:16 +0300

    Product Details

--- Product_Details/script.js
+++ Product_Details/script.js
@@ -3,11 +3,13 @@
 const minus = document.getElementById('minus');
 const plus = document.getElementById('plus');
 const input = document.getElementById('input');
+const minProduct = 1;
+const maxProduct = 9;
 
 minus.addEventListener('click', function(event){
     event.preventDefault();
     let value = input.value - 1;
-    if (input.value <= 1){
+    if (input.value <= minProduct){
         return console.log('You may have at least one item');
     } 
     input.value = value;
@@ -16,7 +18,7 @@ minus.addEventListener('click', function(event){
 plus.addEventListener('click', function(event){
     event.preventDefault();
     let value = Number(input.value) + 1;
-    if (input.value >= 9) {
+    if (input.value >= maxProduct) {
         return console.log('You can take only 9 items')
     }
     input.value = value;
