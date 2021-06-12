8c3428f2f8049ea489807c74a8fe69a4058c9792
andreii1907 <luchianandrei1024@yahoo.com>
2021-06-13 01:23:39 +0300

    changes on assets

--- Assets/mobile-nav.js
+++ Assets/mobile-nav.js
@@ -3,7 +3,7 @@
 const hamburger = document.getElementById('icon');
 const mobileNav = document.getElementById('mobile-nav');
 const cancelIcon = document.getElementById('cancel');
-const enterIcon =document.getElementById('enter');
+const enterIcon = document.getElementById('enter');
 
 enterIcon.addEventListener("click", function(event){
     event.preventDefault;
@@ -17,4 +17,27 @@ cancelIcon.addEventListener("click", function(event){
     mobileNav.classList.toggle("active");
     cancelIcon.style.display = 'none';
     enterIcon.style.display = 'block';
+});
+
+
+// Filter for mobile //
+const mobileFilter = document.getElementById('filtering');
+const upArrow = document.getElementById('up');
+const downArrow = document.getElementById('down');
+const list = document.getElementById('list');
+
+downArrow.addEventListener("click", function(event){
+    event.preventDefault;
+    mobileFilter.style.marginTop = '0';
+    list.style.marginTop = '0';
+    downArrow.style.display = 'none';
+    upArrow.style.display = 'block';
+});
+
+upArrow.addEventListener("click", function(event){
+    event.preventDefault();
+    mobileFilter.style.marginTop = '-700px';
+    list.style.marginTop = '130px';
+    upArrow.style.display = 'none';
+    downArrow.style.display = 'block';
 });
