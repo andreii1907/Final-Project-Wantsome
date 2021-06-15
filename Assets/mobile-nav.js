52167a8d4adcf3debd19b2f809ffe11ea7ccbc9b
andreii1907 <luchianandrei1024@yahoo.com>
2021-06-15 18:07:17 +0300

    mobile-nav functionality

--- Assets/mobile-nav.js
+++ Assets/mobile-nav.js
@@ -18,26 +18,3 @@ cancelIcon.addEventListener("click", function(event){
     cancelIcon.style.display = 'none';
     enterIcon.style.display = 'block';
 });
-
-
-// Filter for mobile //
-const mobileFilter = document.getElementById('filtering');
-const upArrow = document.getElementById('up');
-const downArrow = document.getElementById('down');
-const list = document.getElementById('list');
-
-downArrow.addEventListener("click", function(event){
-    event.preventDefault;
-    mobileFilter.style.marginTop = '0';
-    list.style.marginTop = '0';
-    downArrow.style.display = 'none';
-    upArrow.style.display = 'block';
-});
-
-upArrow.addEventListener("click", function(event){
-    event.preventDefault();
-    mobileFilter.style.marginTop = '-700px';
-    list.style.marginTop = '130px';
-    upArrow.style.display = 'none';
-    downArrow.style.display = 'block';
-});
