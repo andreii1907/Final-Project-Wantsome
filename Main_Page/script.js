95a838d52cfac72b34dd917847b2409442b0267c
andreii1907 <luchianandrei1024@yahoo.com>
2021-06-18 14:32:28 +0300

    Carousel Functionality finished

--- Main_Page/script.js
+++ Main_Page/script.js
@@ -3,7 +3,6 @@ const rightBtn = document.getElementsByClassName('next-slide');
 const cardsMen = document.querySelectorAll('#suggestions-men .suggested-articles .cards');
 const cardsWomen = document.querySelectorAll('#suggestions-women .suggested-articles .cards')
 
-
 function carousel(gender, forword, reverse) {
     let count = 0;
     forword.addEventListener('click', function(e){
@@ -12,13 +11,21 @@ function carousel(gender, forword, reverse) {
         for(let card of gender) {
             card.style.transition = '0.5s linear';
             if(count == 0) card.style.left = '0px';
-            if(count == 1) card.style.left = '-720px';
-            if(count == 2) card.style.left = '-1440px';
-            if(count == 3) card.style.left = '-2160px';
-            if(count > 3){
+            if(count == 1){
+                if(window.innerWidth < 600) {
+                    card.style.left = '-99%';
+                } else card.style.left = '-101.5%';
+            } 
+            if(count == 2){
+                if(window.innerWidth < 600) {
+                    card.style.left = '-199%';
+                } else card.style.left = '-203%';
+            } 
+            if(count > 2){
                 card.style.left = '0px';
                 count = 0;
             } 
+            
         }
     })
 
@@ -27,13 +34,26 @@ function carousel(gender, forword, reverse) {
         console.log('right');
         for(let card of gender) {
             if(count == 0) card.style.left = '0px';
-            if(count == 1) card.style.left = '-720px';
-            if(count == 2) card.style.left = '-1440px';
-            if(count == 3) card.style.left = '-2220px';
+            if(count == 1){
+                if(window.innerWidth < 600) {
+                    card.style.left = '-99%';
+                } else card.style.left = '-101.5%';
+            }
+            if(count == 2){
+                if(window.innerWidth < 600) {
+                    card.style.left = '-199%';
+                } else card.style.left = '-203%';
+            }
             if(count < 0) count = 0;
+
         }
     })
 }
 
 carousel(cardsMen, rightBtn[0], leftBtn[0]);
 carousel(cardsWomen, rightBtn[1], leftBtn[1]);
+
+const buttonForDetails = cardsMen[0].children;
+buttonForDetails[0].addEventListener('click', function(e){
+    window.location.href = '../Product_Details/index.html';
+})
